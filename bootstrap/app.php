91a7e920f6ea8e078d7279d61784cb658acbbd07
SengHour2387 <166191211+SengHour2387@users.noreply.github.com>
2026-03-13 09:31:24 +0700

    Fix mixed content on Railway by trusting proxies

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,10 +11,11 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-    $middleware->alias([
-        'role' => RoleMiddleware::class,
-    ]);
-})
+        $middleware->trustProxies(at: '*');
+        $middleware->alias([
+            'role' => RoleMiddleware::class,
+        ]);
+    })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
     })->create();
